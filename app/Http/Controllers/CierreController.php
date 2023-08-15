dffff3d014945a4d9f17264e57b9d128a9dc6ad5
Marcos Urdaneta <mjuv_1978@hotmail.com>
2023-08-15 07:59:27 -0400

    Update CierreController.php

--- app/Http/Controllers/CierreController.php
+++ app/Http/Controllers/CierreController.php
@@ -51,15 +51,42 @@ class CierreController extends Controller
         $data = Cierre::join('detalle_cierres','detalle_cierres.cierre_id','=','cierres.id')
                 ->select('*')
                 ->get();
-
-                // $res = json_decode($data);
-
                 $this->fpdf = new Fpdf;
-                $this->fpdf->AddPage();
+                $this->fpdf->AddPage('L');
                 $this->fpdf->SetFont('Courier', 'B', 18);
-                $this->fpdf->Cell(50, 25, $data);
+                foreach ($data as $value) {
+                    $this->fpdf->MultiCell(50, 25, $value->supervisor);
+                }
                 return $this->fpdf->Output('F', $path);
                 // return response($data);
                 // exit;
-    }   
+    }
+
+    // public function Header(){
+    //     $this->fpdf = new Fpdf;
+    //     // Select Arial bold 15
+    //     $this->fpdf->SetFont('Arial', 'B', 15);
+    //     // Move to the right
+    //     $this->fpdf->Cell(80);
+    //     // Framed title
+    //     $this->fpdf->Cell(30, 10, 'Title', 1, 0, 'C');
+    //     // Line break
+    //     $this->fpdf->Ln(20);
+    // }
+ 
+}
+
+class PDF extends FPDF
+{
+    function Header()
+    {
+        // Select Arial bold 15
+        $this->SetFont('Arial', 'B', 15);
+        // Move to the right
+        $this->Cell(80);
+        // Framed title
+        $this->Cell(30, 10, 'Title', 1, 0, 'C');
+        // Line break
+        $this->Ln(20);
+    }
 }
